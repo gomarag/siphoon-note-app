64c9a052550da0c9dbea0abc216b800e4669ff0c
gomarag <maragriver@gmail.com>
2023-06-26 14:51:51 +0900

    update: 마이그레이션 파일로 mySql에 테이블 생성 완료
    1) 마이그레이션 파일 생성
    npx sequelize-cli migration:generate --name migration-ver2
    2) 테이블 마이그레이션을 위한 up, down 작성 server/migrations/20230626053821-migration-ver2.js
    3) config 내용대로 mySql에 테이블 생성하기
    npx sequelize-cli db:migrate

--- server/models/init-models.js
+++ server/models/init-models.js
@@ -1,16 +1,12 @@
 var DataTypes = require("sequelize").DataTypes;
 var _essay_tag = require("./essay_tag");
 var _essays = require("./essays");
-var _inspirations = require("./inspirations");
-var _records = require("./records");
 var _tags = require("./tags");
 var _users = require("./users");
 
 function initModels(sequelize) {
   var essay_tag = _essay_tag(sequelize, DataTypes);
   var essays = _essays(sequelize, DataTypes);
-  var inspirations = _inspirations(sequelize, DataTypes);
-  var records = _records(sequelize, DataTypes);
   var tags = _tags(sequelize, DataTypes);
   var users = _users(sequelize, DataTypes);
 
@@ -20,14 +16,10 @@ function initModels(sequelize) {
   tags.hasMany(essay_tag, { as: "essay_tags", foreignKey: "tag_id"});
   essays.belongsTo(users, { as: "user", foreignKey: "user_id"});
   users.hasMany(essays, { as: "essays", foreignKey: "user_id"});
-  records.belongsTo(users, { as: "user", foreignKey: "user_id"});
-  users.hasOne(records, { as: "record", foreignKey: "user_id"});
 
   return {
     essay_tag,
     essays,
-    inspirations,
-    records,
     tags,
     users,
   };
