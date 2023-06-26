7818818f66e113c66af352a584a89f7f6398966c
gomarag <maragriver@gmail.com>
2023-06-26 18:42:57 +0900

    edit: 라우터 경로 단축
    불필요하게 나뉜 단계의 축소
    인덱스 라우터에서 '경로', '호출메소드'를 명시함.
    
    router/index,js -> (users) -> userController

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,43 +1,49 @@
 const express = require('express');
 const router = express.Router();
 const logger = require('../middlewares/logger');
-
+const {
+    signup,
+    // signin,
+    // signout,
+    // deleteUserInfo,
+    // getUserInfo,
+    // updateUserInfo,
+} = require('../controllers/UserController');
 // 1. landing
 // router.get('/', require('./landing'));
 
-router.use(`/signup`, require('./user'));
-router.use(`/signin`, require('./user'));
-router.use(`/signout`, require('./user'));
-router.use(`/usersinfo`, require('./user'));
+router.post('/signup', signup);
+// router.post('/', signin);
+// router.delete('/', signout);
+// router.get('/', getUserInfo);
+// router.patch('/', updateUserInfo);
+// router.delete('/', deleteUserInfo);
 
-// 3. essay
+// router.use(`/signup`, require('./user'));
+// router.use(`/signin`, require('./user'));
+// router.use(`/signout`, require('./user'));
+// router.use(`/usersinfo`, require('./user'));
+
 // router.use(`/essays`, require('./essay'));
 
-// // 4. trash
-// router.use(`/trashes`, require('./trash'));
+// router.use((req, res, next) => {
+//   const err = new Error(`😈 ${req.method} ${req.url} Router Not Found 😈`);
+//   logger.info(err);
+//   logger.error(err);
 
-// // 5. user
+//   err.status = 404;
+//   next(err);
+// });
 
-// // 6. tag
-// router.use(`/tags`, require('./tag'));
+// router.use((err, req, res, next) => {
+//   logger.info(req.method, req.url);
+//   logger.error(err);
 
-router.use((req, res, next) => {
-  const err = new Error(`😈 ${req.method} ${req.url} Router Not Found 😈`);
-
-  logger.error(err);
-
-  err.status = 404;
-  next(err);
-});
-
-router.use((err, req, res, next) => {
-  logger.error(err);
-
-  res.status(err.status || 500);
-  res.json({
-    message: err.message,
-    error: err,
-  });
-});
+//   res.status(err.status || 500);
+//   res.json({
+//     message: err.message,
+//     error: err,
+//   });
+// });
 
 module.exports = router;
