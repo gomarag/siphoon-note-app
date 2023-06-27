dbef28d379366e94437992caf739c001c8ff9ace
gomarag <maragriver@gmail.com>
2023-06-27 20:49:36 +0900

    add: 랜딩페이지 요청에 샘플 응답 코드

--- server/index.js
+++ server/index.js
@@ -30,9 +30,6 @@ app.use(cors(
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
 app.use('/', require('./routes'));
 
 app.use((err, req, res, next) => {
